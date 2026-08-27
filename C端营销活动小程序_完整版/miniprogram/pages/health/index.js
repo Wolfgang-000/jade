dcb28e40bcfd9f75475cb0a5c68eef67caa8d3f9
Wolfgang-000 <2595272752@qq.com>
2026-08-27 09:57:23 +0800

    cleanup: remove deprecated health tab page

--- C端营销活动小程序_完整版/miniprogram/pages/health/index.js
+++
@@ -1,6 +0,0 @@
-const {articles,scenes}=require('../../data/mock');
-Page({
- data:{articles,scenes},
- pharmacist(){wx.navigateTo({url:'/pages/pharmacist/index'})},
- product(){wx.navigateTo({url:'/pages/product-list/index?category='+encodeURIComponent('健康场景')})}
-});
