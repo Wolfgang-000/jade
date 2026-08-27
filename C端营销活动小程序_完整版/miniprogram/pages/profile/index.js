618d5712b90e5d48ae8ea709ddc56b1032895de4
Wolfgang-000 <2595272752@qq.com>
2026-08-27 10:49:21 +0800

    refactor: adapt profile to B2B customer account

--- C端营销活动小程序_完整版/miniprogram/pages/profile/index.js
+++ C端营销活动小程序_完整版/miniprogram/pages/profile/index.js
@@ -1,6 +1,6 @@
 Page({
- data:{member:getApp().globalData.member},
+ data:{customer:getApp().globalData.customer},
+ onShow(){this.setData({customer:getApp().globalData.customer})},
  nav(e){wx.navigateTo({url:e.currentTarget.dataset.url})},
- orders(){wx.navigateTo({url:'/pages/orders/index'})},
- member(){wx.navigateTo({url:'/pages/member/index'})}
+ orders(){wx.navigateTo({url:'/pages/orders/index'})}
 });
