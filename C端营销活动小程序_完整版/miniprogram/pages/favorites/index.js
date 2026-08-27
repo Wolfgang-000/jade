e41188f7d4f283884f9470d007c6618425da8d6d
Wolfgang-000 <2595272752@qq.com>
2026-08-27 10:52:56 +0800

    refactor: add frequent products by B2B minimum quantity

--- C端营销活动小程序_完整版/miniprogram/pages/favorites/index.js
+++ C端营销活动小程序_完整版/miniprogram/pages/favorites/index.js
@@ -1,7 +1,13 @@
-const {products}=require('../../data/mock');const store=require('../../utils/store');
+const {products}=require('../../data/mock');
+const store=require('../../utils/store');
 Page({
  data:{list:[]},
  onShow(){const ids=store.getFavorites();this.setData({list:ids.map(id=>products.find(p=>p.id===id)).filter(Boolean)})},
  go(e){wx.navigateTo({url:'/pages/product-detail/index?id='+e.currentTarget.dataset.id})},
- cart(e){const p=products.find(x=>x.id===e.currentTarget.dataset.id);store.addCart(p);wx.showToast({title:'已加入购物袋'})}
+ cart(e){
+   const p=products.find(x=>x.id===e.currentTarget.dataset.id);
+   if(!p)return;
+   store.addCart(p,p.moq||1,p.spec);
+   wx.showToast({title:'已加入采购单'});
+ }
 });
