1469b1b0028d2584d16547350d4d522ed09859c6
Wolfgang-000 <2595272752@qq.com>
2026-08-27 10:50:53 +0800

    cleanup: remove unused consumer filter logic

--- C端营销活动小程序_完整版/miniprogram/pages/product-list/index.js
+++ C端营销活动小程序_完整版/miniprogram/pages/product-list/index.js
@@ -1,8 +1,7 @@
 const {products}=require('../../data/mock');
 Page({
- data:{category:'精选商品',list:products,sort:'综合'},
- onLoad(o){const c=decodeURIComponent(o.category||'精选商品');this.setData({category:c,list:products})},
+ data:{category:'采购商品',list:products,sort:'综合'},
+ onLoad(o){const c=decodeURIComponent(o.category||'采购商品');this.setData({category:c,list:products})},
  goProduct(e){wx.navigateTo({url:'/pages/product-detail/index?id='+e.currentTarget.dataset.id})},
- setSort(e){this.setData({sort:e.currentTarget.dataset.sort})},
- filter(){wx.showToast({title:'筛选：品牌/价格/剂型/规格/人群/OTC',icon:'none'})}
+ setSort(e){this.setData({sort:e.currentTarget.dataset.sort})}
 });
