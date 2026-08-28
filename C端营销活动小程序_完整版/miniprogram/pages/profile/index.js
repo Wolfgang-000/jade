4c078c184adf5bf2a683115cc443c47734f0bcd8
Wolfgang-000 <2595272752@qq.com>
2026-08-28 16:22:55 +0800

    fix: switch order navigation to tabBar

--- C端营销活动小程序_完整版/miniprogram/pages/profile/index.js
+++ C端营销活动小程序_完整版/miniprogram/pages/profile/index.js
@@ -2,5 +2,5 @@ Page({
  data:{customer:getApp().globalData.customer},
  onShow(){this.setData({customer:getApp().globalData.customer})},
  nav(e){wx.navigateTo({url:e.currentTarget.dataset.url})},
- orders(){wx.navigateTo({url:'/pages/orders/index'})}
+ orders(){wx.switchTab({url:'/pages/orders/index'})}
 });
