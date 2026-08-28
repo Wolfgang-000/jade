949df16e01a5b0ba56bfd43964271cb3af2c5faa
Wolfgang-000 <2595272752@qq.com>
2026-08-28 16:37:06 +0800

    cleanup: remove deprecated consumer member page

--- C端营销活动小程序_完整版/miniprogram/pages/member/index.js
+++
@@ -1,7 +0,0 @@
-Page({
- data:{member:getApp().globalData.member},
- coupons(){wx.navigateTo({url:'/pages/coupons/index'})},
- points(){wx.navigateTo({url:'/pages/points/index'})},
- activity(){wx.navigateTo({url:'/pages/activity/list/index'})},
- service(){wx.navigateTo({url:'/pages/service/index'})}
-});
